4b18c64e4008e50d3898caaca4239b19e96110ce
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-11-23 16:28:04 +0530

    faqs-section added

--- static/js/script.js
+++ static/js/script.js
@@ -27,3 +27,24 @@ const interval = setInterval(() =>{
 }, intervalTime);
 
 
+
+// faq-animation
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq)=>{
+    console.log('clikced');
+    faq.addEventListener("click", ()=>{
+      if(faq.classList.contains("active")){
+        faq.classList.remove("active");
+      }else{
+        faq.classList.add("active");
+        faqs.forEach((otherFaq)=>{
+          if(otherFaq!= faq){
+            otherFaq.classList.remove("active");
+          }
+        })
+      }
+    })
+})
+
