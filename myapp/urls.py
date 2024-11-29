70289d543a29129623106702c9a99262502c4499
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-11-29 10:51:25 +0530

    dashboard.html created

--- myapp/urls.py
+++ myapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('login',views.login_view, name ='login'),
     path('logout',views.logout_view, name ='logout'),
     path('register',views.register, name ='register'),
+    path('dashboard',views.dashboard, name ='dashboard'),
     path('portfolio',views.portfolio_view, name ='portfolio'),
     path('create_trade',views.create_trade, name ='create_trade'),
     path('user_trades',views.user_trades, name ='user_trades'),
