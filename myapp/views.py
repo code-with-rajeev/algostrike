6ab251ca7b50db49a862f5dd7c15c4458b98bfa7
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-11-29 10:51:25 +0530

    dashboard.html created

--- myapp/views.py
+++ myapp/views.py
@@ -29,7 +29,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('portfolio')  # Redirect to portfolio page after login
+            return redirect('dashboard')  # Redirect to portfolio page after login
         else:
             messages.error(request, "Invalid username or password.")
     return render(request, 'login.html')
@@ -38,6 +38,41 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        # Check for existing data
+        if CustomUser.objects.filter(email__iexact=email).exists():
+            return render(request, 'register.html',{'message':'Email already exists'})
+        if CustomUser.objects.filter(username=username).exists():
+            return render(request, 'register.html',{'message':'Username already exists'})
+
+
+
+        # Create and save the new user with default PnL and Invested
+        try:
+            new_user = CustomUser.objects.create(
+            username=username,
+            email=email,
+            password=make_password(password),  # Hash the password
+            )
+            new_user.save()            
+        except IntegrityError:
+            return render(request, 'register.html',{'message':'Something Went Wrong'})
+        # Redirect to the login page
+        return redirect('login')
+
+    return render(request, 'register.html',{'message':'Please enter unique credentials only'})
+
+@login_required
+def dashboard(request):
+    #trades = TradeLog.objects.all()
+    #return render(request, 'portfolio.html', {'trades': trades})
+    if not request.user.is_authenticated:
+        return redirect(f'/login/?next={request.path}')
+    return render(request, 'dashboard.html')
 
 @login_required
 def portfolio_view(request):
@@ -75,33 +110,4 @@ def user_trades(request):
     trades = TradeLog.objects.filter(user=request.user)  # Fetch all trade records
     return render(request, 'user_trades.html', {'trades': trades})
     """
-
-def register(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        # Check for existing data
-        if CustomUser.objects.filter(email__iexact=email).exists():
-            return render(request, 'register.html',{'message':'Email already exists'})
-        if CustomUser.objects.filter(username=username).exists():
-            return render(request, 'register.html',{'message':'Username already exists'})
-
-
-
-        # Create and save the new user with default PnL and Invested
-        try:
-            new_user = CustomUser.objects.create(
-            username=username,
-            email=email,
-            password=make_password(password),  # Hash the password
-            )
-            new_user.save()            
-        except IntegrityError:
-            return render(request, 'register.html',{'message':'Something Went Wrong'})
-        # Redirect to the login page
-        return redirect('portfolio')
-
-    return render(request, 'register.html',{'message':'Please enter unique credentials only'})
-
-
+    return render(request, 'user_trades.html') #remove it later
