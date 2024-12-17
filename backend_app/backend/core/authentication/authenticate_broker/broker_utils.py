37a4340b345c71bcb60ea566a7e69ee239527595
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-12-17 14:05:25 +0530

    updated broker_utils.py

--- backend_app/backend/core/authentication/authenticate_broker/broker_utils.py
+++ backend_app/backend/core/authentication/authenticate_broker/broker_utils.py
@@ -4,12 +4,12 @@ Helper / neccessary functions that may be used while authentication / login / ot
 def requried_credentials(credentials):
     
     # Temporarily redirect under maintainance response
-    return JsonResponse({'success': False, 'message': 'This service is under maintainance, Please try again later!'}, status=401)
+    return {'success': False, 'message': 'This service is under maintainance, Please try again later!', status=401}
 
 def does_broker_exist(credentials):
     message = 'Some error occured' # Temporary response to avoid further processing
-    return JsonResponse({'success': False, 'message': message}, status=400)
+    return {'success': False, 'message': message, status=400}
 
 def route_broker(credentials):
     message = 'Some error occured' # Temporary response to avoid further processing
-    return JsonResponse({'success': False, 'message': message}, status=400)
+    return {'success': False, 'message': message, status=400}
