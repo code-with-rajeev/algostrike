03c9c053d03fa5d8efebca0fe371c5ba5be7dd7a
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-12-17 14:03:27 +0530

    updated authenticate.py

--- backend_app/backend/core/authentication/authenticate_broker/authenticate.py
+++ backend_app/backend/core/authentication/authenticate_broker/authenticate.py
@@ -2,7 +2,6 @@
 Description :
 
 """
-from django.http import JsonResponse
 from broker_utils import *
 # importing required_credentials, does_broker_exist, route_broker, get_credentials
 
@@ -10,23 +9,23 @@ def authenticate(credentials):
 
     # check for missing credentials !
     response = requried_credentials(credentials)
-    if (not response.success):
-        return JsonResponse({'success': False, 'message': 'Missing required fields'}, status=400)
+    if (not response['success']):
+        return {'success': False, 'message': 'Missing required fields',status=400}
     
     # check whether, broker is already registered. No need to store credentials in database. Update logs and session after authenticating successfully. 
     response = does_broker_exist(credentials)
-    if (not response.success):
+    if (not response['success']):
         # optional: If already exits, try to renew session/ login-again. Avoid data redudancy.
-        return JsonResponse({'success': False, 'message': 'Broker already exist'}, status=400)
+        return {'success': False, 'message': 'Broker already exist', status=400}
 
     #  route broker's credentials to their appropriate authentication / login method
     response = route_broker(credentials)
-    if (not response.success):
+    if (not response['success']):
         """
         return response
         """
         message = 'Invalid Credentials' # Temporary response
-        return JsonResponse({'success': False, 'message': message}, status=400)
-    return JsonResponse({'success': True})
+        return {'success': False, 'message': message, status=400}
+    return {'success': True}
         
     
