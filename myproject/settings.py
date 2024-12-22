dd6b4e7f063ffea1fb2f6331d1376360319124e1
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-12-22 10:41:01 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+APPEND_SLASH = True
 
 ALLOWED_HOSTS = ["*","127.0.0.1","localhost","10.0",".vercel.app"]
 
