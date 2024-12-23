0c9c9e35b0234985bf607b3320252d27efdf52b7
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-12-23 12:08:06 +0530

    debugging

--- backend_app/urls.py
+++ backend_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   """
+   
  # Note: Make sure to redirect these URL from frontend to backend. Temporary Backend and Frontend are running on same server Vercel.
-    path('/broker_credentials/',views.broker_credentials, name ='broker_credentials'),
-    path('/verify_broker/',views.verify_broker, name ='verify_broker'),
-    """
+    path('broker_credentials/',views.broker_credentials, name ='broker_credentials'),
+    path('verify_broker/',views.verify_broker, name ='verify_broker'),
+    
     ]
