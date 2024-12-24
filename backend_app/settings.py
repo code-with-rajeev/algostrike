1ba3a875fcab4fc02c9dbddfe12048ec6b1529ee
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-12-24 13:35:43 +0530

    adding SERVER_IP in ALLOWED_HOSTS in settings.py

--- backend_app/settings.py
+++ backend_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["www.algostrike.com", "algostrike.com"]
+ALLOWED_HOSTS = ["www.algostrike.com", "algostrike.com",os.environ.get('SERVER_IP')]
 
 
 # Application definition
