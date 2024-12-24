b03c7041def820e56e982424fb0957f82270c315
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2024-12-24 13:30:41 +0530

    updated urls.py

--- backend_app/urls.py
+++ backend_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
    
  # Note: Make sure to redirect these URL from frontend to backend. Temporary Backend and Frontend are running on same server Vercel.
-    path('broker_credentials/',views.broker_credentials, name ='broker_credentials'),
-    path('verify_broker/',views.verify_broker, name ='verify_broker'),
-    path('test_server/',views.test_server, name ='test_server'),
+    path('broker_credentials',views.broker_credentials, name ='broker_credentials'),
+    path('verify_broker',views.verify_broker, name ='verify_broker'),
+    path('test_server',views.test_server, name ='test_server'),
     ]
