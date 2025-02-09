769594debb388aafebc06b9e39a17f40a98e558f
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-02-10 02:20:37 +0530

    celery.py added

---
+++ backend_app/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','backend_app.settings')
+
+app = Celery('backend_app')
+app.conf.enable_utc = False
+
+app.conf.update(timezone = 'Asia/Kolkata')
+
+app.config_from_object(settings, namespace='CELERY')
+
+# Celery Beat Settings
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
