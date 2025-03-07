8aa91f6eb3e85f946d8a197013209d90904a9f9d
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-03-07 12:57:21 +0530

    updated algo_scheduler .py

--- backend_app/backend/tasks/premarket/algo_scheduler.py
+++ backend_app/backend/tasks/premarket/algo_scheduler.py
@@ -7,7 +7,18 @@ from datetime import date
 #logger = get_logger('scheduler')
 
 @shared_task
-def filer_algos():
+def algo_scheduler():
+    """
+    PREMARKET TASK: activate all inactive algos and store in redis
+    """
+    pass
+
+
+@shared_task
+def filter_all_algos():
+    """
+    At any instance, Returns current status of Algos
+    """
     cache = CacheManager()
     today = date.today().isoformat()  # e.g., "YYYY-MM-DD"
     try:
@@ -18,15 +29,27 @@ def filer_algos():
         disabled_algos = Algo.objects.filter(is_active__iexact='disabled') # algo is under maintainance
         # is_active == "disabled", means algo under maintainance.
         active_algos = {}
+        inactive_algos = {}
+        disabled_algos = {}
+
         for algo in inactive_algos:
+            algo_id = algo.algo_id  # e.g., "algo1"
+            algo_name = algo.name
+            inactive_algos[algo_id].append(algo_name)
+
+        for algo in active_algos:
             algo_id = algo.algo_id  # e.g., "algo1"
             algo_name = algo.name
             active_algos[algo_id].append(algo_name)
 
+        for algo in disabled_algos:
+            algo_id = algo.algo_id  # e.g., "algo1"
+            algo_name = algo.name
+            disabled_algos[algo_id].append(algo_name)
             # Store in Redis: {algo1: [user1, user2], algo2: [user3]}
-            cache.set(f"active_algos:algo:{algo_name}", {"status":"active"})
+            #cache.set(f"active_algos:algo:{algo_name}", {"status":"active"})
             #logger.info(f"Scheduled algos for {today}: {active_algos.keys()}")
-        return active_algos
+        return {"inactive_algos":inactive_algos,"active_algos":active_algos,"disabled_algos":disabled_algos}
     except Exception as e:
         #logger.error(f"Failed to schedule algos: {str(e)}")
         return {}
