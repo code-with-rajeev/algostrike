7053c1bfde61cd32464a9dc822048da2929aefec
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-03-17 09:30:35 +0530

    Testing config.py

---
+++ backend_app/backend/algos/9_26_EMA_Crossover/config.py
@@ -0,0 +1,20 @@
+"""
+Configuration
+"""
+ABOUT = {
+    
+    "name":"9_26_EMA_Crossover",
+    "type":"Demo",
+    "author":"Rajeev"
+    
+    }
+STATIC_REQUIREMENTS = [
+    """
+    Testing: SUB, SNAP, API method of fetching data
+    """
+    {
+        "instrument":"RELIANCE",
+        "timeframe":"5M",
+        "frequency":"API" #Major Delay Response
+    }
+]
