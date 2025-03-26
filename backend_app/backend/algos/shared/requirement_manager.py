c2e072d7e0674f2460cb58a9f0ebfc7e8cd1c05a
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-03-26 18:03:41 +0530

    updated requirement_manager.py

--- backend_app/backend/algos/shared/requirement_manager.py
+++ backend_app/backend/algos/shared/requirement_manager.py
@@ -1,6 +1,5 @@
-from celery import shared_task
-from myapp.models import Algo  # Adjust your ORM model
 from backend.interfaces.cache_manager import CacheManager
+import json
 #from backend.core.uttils import get_logger
 
 #logger = get_logger('app')
@@ -13,23 +12,27 @@ class RequirementManager:
     def __init__(self):
         self.cache = CacheManager()
         
-    def filter_requirements(REQUIREMENTS):
-        # StreamSubscriber format
-        # for GLOBAL : "active_algos:algo:requirements:GLOBAL" : {f"{INS}":
+    def filter_requirements(self,REQUIREMENTS):
+        # StreamSubscriber format: hset
+        # for GLOBAL : "active_algos:algo:requirements:GLOBAL" : {f"{INS}":[timeframe,last_fetch]}
         pass
         
-    def route_requirements(REQUIREMENTS):
-        routes = {}
+    def route_requirements(self,REQUIREMENTS):
+        """ stores and manages the mapping of requirements to corresponding algorithms """
+        filtered_routes = {}
+        handler = self.cache.handler()
+        key = f"active_algos:algo:requirements:route"
         # {'NIFTY:1M': ['algo1', 'algo3'], 'ZOMATO:5M': ['algo1', 'algo3'], 'NIFTY:5M': ['algo2'], 'RELIANCE:15M': ['algo2']}
         for algo  in REQUIREMENTS:
-            req = algo['req']
-            for r in req:
-                key = f"{r['instrument']}:{r['timeframe']}"
-                if routes.get(key, None):
-                    routes[key].append(algo['algo_name'])
+            static_requirements = algo['requirements']
+            for r in static_requirements:
+                symbol = f"{r['instrument']}:{r['timeframe']}"
+                if filtered_routes.get(symbol, None):
+                    filtered_routes[symbol].append(algo['algo_name'])
                 else:
-                    routes[key] = [algo['algo_name']]
+                    filtered_routes[symbol] = [algo['algo_name']]
 
         #add in redis
-        self.cache.set(f"active_algos:algo:requirements:route", routes, ttl=86400)  # 24-hour TTL
-        return routes
+        for requirement,routes in filtered_routes.items():
+            handler.hset(key, requirement,json.dumps(routes))
+            handler.expire(key,86400)# TTL: 24 Hours
