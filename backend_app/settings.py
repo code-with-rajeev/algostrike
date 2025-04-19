eb48fd3a19a9d5278a52e7c30391b24248dfd004
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-04-19 10:15:37 +0530

    updated settings,py

--- backend_app/settings.py
+++ backend_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["www.algostrike.com", "algostrike.com",os.environ.get('SERVER_IP')]
+ALLOWED_HOSTS = ["www.algostrike.com", "algostrike.com",os.environ.get('SERVER_IP'),"app.algostrike.com"]
 
 # CELERY SETTINGS
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
@@ -69,6 +69,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+    "https://www.algostrike.com",
+    "https://algostrike.com",
 ]
 """
 # Optionally, allow specific HTTP methods (default includes GET, POST, etc.)
