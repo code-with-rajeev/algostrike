2bb1c18ede414174cf247de02007c2be68e7ce4d
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-05-06 12:37:13 +0530

    added notifications.py [ Implemented OTP based Email ]

--- backend_app/backend/core/notifications.py
+++ backend_app/backend/core/notifications.py
@@ -9,8 +9,8 @@ import os
 # Email configuration
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_USER =os.environ.get("TEST_MAIL")
-EMAIL_PASS = os.environ.get("TEST_MAIL_PASSWORD")
+EMAIL_USER = os.environ.get("HOST_MAIL")
+EMAIL_PASS = os.environ.get("HOST_MAIL_PASSWORD")
 
 def send_email(to_email, subject, message):
     """
@@ -27,7 +27,30 @@ def send_email(to_email, subject, message):
             server.login(EMAIL_USER, EMAIL_PASS)
             server.sendmail(EMAIL_USER, to_email, msg.as_string())
 
-        print(f"Email sent to {to_email}")
+        #print(f"Email sent to {to_email}")
 
     except Exception as e:
-        print(f"Failed to send email: {str(e)}")
+        pass
+        #print(f"Failed to send email: {str(e)}")
+
+def authentication_otp_mail(username, to_email, otp):
+    """
+    Sends an OTP via email notification.
+    """
+    message = f"Dear {username},\nYour One-Time Password(OTP) for Algostrike is {otp}.\nThis OTP is valid for 5 minutes. Please keep it confidenial for security reasons.\nIf you did not request this code, you can safely ignore this email.\nBest regards,\nTeam Algostrike\nwww.algostrike.com"
+    try:
+        msg = MIMEText(message)
+        msg["Subject"] = "Your AlgoStrike verification OTP"
+        msg["From"] = EMAIL_USER
+        msg["To"] = to_email
+
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_USER, EMAIL_PASS)
+            server.sendmail(EMAIL_USER, to_email, msg.as_string())
+
+        #print(f"Email sent to {to_email}")
+
+    except Exception as e:
+        pass
+        #print(f"Failed to send email: {str(e)}")
