316693014e89a4cc9622aa6bdfba95a8617ce321
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-05-06 14:11:27 +0530

    updated urls.py [ Added new API ]

--- backend_app/urls.py
+++ backend_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
    
  # Note: Make sure to redirect these URL from frontend to backend. Temporary Backend and Frontend are running on same server Vercel.
-    path('broker_credentials',views.broker_credentials, name ='broker_credentials'),
-    path('verify_broker',views.verify_broker, name ='verify_broker'),
-    path('',views.test_server, name ='test_server'),
+    path('api/broker/authenticate/broker_credentials',views.broker_credentials, name ='broker_credentials'),
+    path('api/broker/authenticate/verify_broker',views.verify_broker, name ='verify_broker'),
+    path('api/test/test_server',views.test_server, name ='test_server'),
+    path('api/test/debug_mode',views.debug_mode, name ='debug_mode'),
+    path('api/user/authenticate/generate_otp',views.generate_otp, name ='generate_otp'),
+    path('api/user/authenticate/verify_otp',views.verify_otp, name ='verify_otp'),
     path('admin/', admin.site.urls),
     ]
