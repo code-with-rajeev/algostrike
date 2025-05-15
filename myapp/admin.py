f68f12a6210de114616bdf233af931eac3fa4187
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-05-15 12:16:11 +0530

    updated admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import CustomUser, Subscription, Strategy, StrategyTradeLog, TradeLog, Transaction
+from .models import CustomUser, Subscription, Strategy, StrategyTradeLog, TradeLog, Transaction, Plans, UserPlanSubscription
 #register model to show it in admin panel OPTIONAL
 admin.site.register(CustomUser)
 admin.site.register(Subscription)
@@ -10,3 +10,5 @@ admin.site.register(Strategy)
 admin.site.register(StrategyTradeLog)
 admin.site.register(TradeLog)
 admin.site.register(Transaction)
+admin.site.register(Plans)
+admin.site.register(UserPlanSubscription)
