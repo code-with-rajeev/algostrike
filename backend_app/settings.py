915bf327fb713ff79bd9bbb2792a1f8d360e7015
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-05-16 14:55:41 +0530

    updated [bug fixes]

--- backend_app/settings.py
+++ backend_app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'channels'
+    'channels',
     'corsheaders',
     'backend_app',
     'myapp',
