c5c8cfe7ec222ef0aca1425ca6d3a6dafec742eb
code-with-rajeev <68783059+code-with-rajeev@users.noreply.github.com>
2025-09-22 19:43:14 +0530

    updated

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
  #' ' empty means main site/project
-    #path('',views.index, name ='index'),
-    #path('home',views.home, name ='home'),
+    path('',views.index, name ='index'),
+    path('home',views.home, name ='home'),
     path('features',views.feature, name ='features'),
     path('guide',views.guide, name ='guide'),
     path('contact',views.contact_us, name ='contact_us'),
